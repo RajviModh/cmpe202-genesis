e8cf61880de64c8c301562ee16c556d4904bb7ab
rajbaraiyaa <rajmansukhbhai.baraiya@sjsu.edu>
2017-10-26 23:13:28 -0700

    Adding Initial code for first player snake

---
+++ Coding/Snake.java
@@ -0,0 +1,46 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Snake here.
+ * 
+ * @author (Genesis) 
+ * @version (10.26.17)
+ */
+public class Snake extends Actor
+{
+    /**
+     * Act - do whatever the Player wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    public Snake()  //initialize player in game
+    {
+        
+    }
+	
+	//to be implemented for checking state of game whether paused or on
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+	
+	//logic for snake eating food
+	public void eat()
+	{
+
+	}
+
+	//logic for health checking and regaining it.
+	public void Life()
+	{
+	
+	}
+
+	//logic for controlling the snake
+	public void controls()
+	{
+	
+	}
+
+	 
+}
