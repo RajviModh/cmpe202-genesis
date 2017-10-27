dd4f4003afb641dfb0b430723b3d72b27558e0fb
shahkevaln <Keval#28121993>
2017-10-26 23:13:14 -0700

    EnemySnake Class Creation

---
+++ Coding/EnemySnake.java
@@ -0,0 +1,47 @@
+import greenfoot.*;
+import java.awt.Color;  
+import java.awt.Font;  
+import java.util.Calendar;
+import java.lang.Math;
+/**
+ * Write a description of class Enemy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class EnemySnake
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Enemy
+     */
+    public EnemySnake()
+    {
+       //Used to draw the Enemies image
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    
+    public void act(){
+    //main method which calls other methods to perform various operations
+    
+    }
+    public void controls()
+    {
+        //Used to control the enemy snake
+        // put your code here
+       
+    }
+    
+     public void eat()
+    {
+        //Called when the snake eats the food
+    }
+}
