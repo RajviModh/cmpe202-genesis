036d0c8512eae47b5f1c9b8e0e89c77d3f6ca2f9
nsjethani <neha.jethani@sjsu.edu>
2017-10-27 23:17:45 -0700

    Adding code for Snake Body

--- Coding/SnakeBody.java
+++ Coding/SnakeBody.java
@@ -1,5 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.*; 
+import java.awt.*; 
 /**
  * Write a description of class SnakeBody here.
  * 
@@ -16,9 +17,13 @@ public class SnakeBody extends Actor
     public static int y=210;
     public static int z=210;
     public int shield_diffrence =2;
+    GreenfootImage defaultBody = new GreenfootImage(Background.playerSize, Background.playerSize); 
+    GreenfootImage Body = new GreenfootImage(Background.playerSize, Background.playerSize); 
+
     
     public SnakeBody()
     {
+        BodyPaint();
         Image();
     }
     
@@ -51,5 +56,37 @@ public class SnakeBody extends Actor
             end_timer=end_timer-80;
         }
         
+    }
+    
+    public void BodyPaint()
+    {
+       //This class gives different size to snake according to shield's value
+       Body.setColor(new Color(0,0,0));
+       Body.fillOval(0, 0, Background.playerSize, Background.playerSize);
+       
+       if(shield==1)
+        {  
+            Body.setColor(new Color(146,123,81));
+            Body.drawOval(0, 0, Background.playerSize-1, Background.playerSize-1);
+            Body.drawOval(0, 0, Background.playerSize-2, Background.playerSize-2);
+        }
+        else if(shield==2)
+        {
+            Body.setColor(new Color(140,120,83));
+            Body.drawOval(0, 0, Background.playerSize-1, Background.playerSize-1);
+            Body.drawOval(0, 0, Background.playerSize-2, Background.playerSize-2);
+        }
+       else if(shield==3)
+       {
+           Body.setColor(new Color(230,232,250));
+            Body.drawOval(0, 0, Background.playerSize-1, Background.playerSize-1);
+            Body.drawOval(0, 0, Background.playerSize-2, Background.playerSize-2);
+       }
+       else if(shield==4)
+       {
+           Body.setColor(new Color(84,84,84));
+            Body.drawOval(0, 0, Background.playerSize-1, Background.playerSize-1);
+            Body.drawOval(0, 0, Background.playerSize-2, Background.playerSize-2);
+       }
     }
 }
