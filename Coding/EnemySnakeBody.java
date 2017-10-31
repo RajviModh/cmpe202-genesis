622d560874a19e7a0a19b044ecf9728bd24fc404
RajviModh <31807189+RajviModh@users.noreply.github.com>
2017-10-30 23:12:51 -0700

    Added EnemySnakeBody bodycolor

--- Coding/EnemySnakeBody.java
+++ Coding/EnemySnakeBody.java
@@ -17,15 +17,55 @@ public class EnemySnakeBody extends Actor
      * Act - do whatever the EnemySnakeBody wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+
+    public static int armour;
+    GreenfootImage Body = new GreenfootImage(Back.playerSize, Back.playerSize);
     
    public void EnemySnakebody()
     {
         //Constructor creation for EnemySnakeBody
-        
+        DrawImages();
     }
     public void act() 
     {
         // Add your action code here.
+    }
+
+    public void DrawImages()
+    {
+        Body.setColor(new Color(255,0,0));
+        switch(armour)
+        {
+            case 0:
+            break;
+            //Leather
+            case 1:
+            Body.setColor(new Color(146,123,81));
+            break;
+             //Bronze
+            case 2:
+            Body.setColor(new Color(140,120,83));
+            break;
+            //Silver
+            case 3:
+            Body.setColor(new Color(230,232,250));
+            break;
+            //Steel
+            case 4:
+            Body.setColor(new Color(84,84,84));
+            break;
+            //Gold
+            case 5:
+            Body.setColor(new Color(217,217,25));
+            break;
+            //Diamond
+            case 6:
+            Body.setColor(new Color(14,191,233));
+            break;
+            //Crystal
+            case 7:
+            Body.setColor(new Color(14,191,233));
+            break;
     } 
    
 }
