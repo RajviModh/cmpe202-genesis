3cc7fda6157b518006deac3db11ecce7b053bfdc
RajviModh <31807189+RajviModh@users.noreply.github.com>
2017-11-06 23:13:08 -0800

    Added attacke method for EnemySnake

---
+++ Coding/EnemySnakeHub.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class EnemySnakeHub here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class EnemySnakeHub extends Actor
+{
+    /**
+     * Act - do whatever the EnemySnakeHub wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    public static int score;
+    public int trans;
+    GreenfootImage image = new GreenfootImage(120,500);
+    
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
