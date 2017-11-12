86d617688d456ab3842278a62a6f7426f1419deb
nsjethani <neha.jethani@sjsu.edu>
2017-11-11 21:17:41 -0800

    Adding FramePerSec class

---
+++ Coding/FramePerSec.java
@@ -0,0 +1,18 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class FramePerSec here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FramePerSec extends Actor
+{
+    public enum Mode {AVERAGE, REALTIME}   
+    public static final Mode mode = Mode.AVERAGE;
+    
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
