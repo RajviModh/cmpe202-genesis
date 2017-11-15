19392048017c49f10ec3c562061dae280b7a8bb7
Divyang Soni <31805433+divyang8842@users.noreply.github.com>
2017-11-14 23:24:28 -0800

    Frame Per Second Checkin

--- Coding/FramePerSec.java
+++ Coding/FramePerSec.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.awt.Color;
 /**
  * Write a description of class FramePerSec here.
  * 
@@ -10,9 +10,44 @@ public class FramePerSec extends Actor
 {
     public enum Mode {AVERAGE, REALTIME}   
     public static final Mode mode = Mode.AVERAGE;
+    private static final Color textColor = Color.BLACK;
+    private long acts_Count;
+    private static final String prefix = "FramePerSec: ";
+    private static final int updateFreq = 20;
+    private int nCount;
+    
+    public FramePerSec()
+    {
+        acts_Count = 0;
+        setImage(new GreenfootImage(150, 16));
+        GreenfootImage image = getImage();
+        updateImage("-");
+    }
+    
+        public void FPSextremes()
+    {
+        nCount++;
+        if(nCount>100)
+        {
+
+            
+        }
+    }
     
     public void act() 
     {
         // Add your action code here.
     }    
+    
+    public double getFPS()
+    {
+        return 0.00;
+    }
+    
+     private final void updateImage(String value)
+    {
+        getImage().clear();
+        GreenfootImage txt = new GreenfootImage(prefix +value, 16, textColor, null);
+        getImage().drawImage(txt, 1, 1);
+    }
 }
